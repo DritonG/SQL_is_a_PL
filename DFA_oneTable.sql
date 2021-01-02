b02e87d94e4950bf26e57f5b8656ce91d60b430d
DritonG <driton-goxhufi@web.de>
2021-01-02 21:31:30 +0100

    Update the query for the transitive envelope and adding a function to execute the query according to an input.

--- DFA_oneTable.sql
+++ DFA_oneTable.sql
@@ -36,23 +36,22 @@ INSERT INTO
     dfa_t(state_id, letter, successor, end_state)
 VALUES
 -- reference node to declare starting dfa_t, every node which is connected to this node is a starting state
-    (0, '', 1, FALSE), -- TODO: !TEST NECESSARY! if 'NULL' as value for start dfa_t is a problem during the process
-    (0, '', 5, FALSE),
-    (1,'F', 2, FALSE), -- start state
-    (2,'o', 3, FALSE),
-    (3,'t', 4, FALSE),
-    (4,'o', NULL, TRUE), -- end state
-    (4,'o', 7, FALSE),
-    (5,'P', 6, FALSE), -- start state
-    (6,'h', 2, FALSE),
-    --(6,'h', NULL, TRUE), -- end state
-    (7,'g', 8, FALSE),
-    (8,'r', 9, FALSE),
-    (9,'a', 10, FALSE),
-    (9,'a', 11, FALSE),
-    (10,'f', NULL, TRUE),
-    (11,'p', 12, FALSE)
-   ,(12,'h', NULL, TRUE)
+    --(0, '', 1, FALSE), -- TODO: !TEST NECESSARY! if 'NULL' as value for start dfa_t is a problem during the process
+    --(0, '', 5, FALSE),
+    (0,'F', 1, FALSE), -- start state
+    (1,'o', 2, FALSE),
+    (2,'t', 3, FALSE),
+    (3,'o', 4, TRUE), -- end state
+    --(3,'o', 7, FALSE),
+    (0,'P', 5, FALSE), -- start state
+    (5,'h', 6, FALSE),
+    (6,'o', 2, FALSE), -- end state
+    (4,'g', 7, FALSE),    (7,'r', 8, FALSE),
+    (8,'a', 9, FALSE),
+    --(8,'a', 11, FALSE),
+    (9,'f', 10, TRUE),
+    (9,'p', 11, FALSE)
+   ,(11,'h', 12, TRUE)
     --(42, NULL, NULL, FALSE) -- TODO: !TEST NECESSARY! if 'NULL' as value for end dfa_t is a problem during the process
 RETURNING *;
 
@@ -62,7 +61,6 @@ RETURNING *;
 -- Tuple-s_0: {1, 5}; dfa_t which predecessor is the node with state_id = 0 are start dfa_t(nodes)
 -- Tuple-F: {4, 6, 10}; dfa_t which successor is the node with state_id = 42 are end dfa_t(nodes)
 -- End
-
 -- Tuple-s_o: list all start dfa_t(nodes)
 SELECT *
     FROM dfa_t
@@ -71,44 +69,81 @@ WHERE state_id = 0;
 SELECT *
     FROM dfa_t
 WHERE end_state = TRUE;
---WHERE successor is null;
 
--- query to get ALL valid 'words', their 'path_string' and 'distance' within the dfa_t.
+-- query to get ALL valid 'words', their 'path_string' and 'distance' within the dfa_t. "TRANSITIVE-HÜLLE"
 WITH RECURSIVE query(id, l , suc, end_s, path_string, distance, word) AS (
   SELECT
          state_id as id,
          letter as l,
          successor as suc,                                   -- sb = the successor
          end_state as end_s,
-         state_id ||'' AS path_string,    -- concatinating the first state with its successor
-         0 AS distance
-        ,''|| letter AS  word
+         state_id ||'-'||successor AS path_string,    -- concatinating the first state with its successor
+         1 AS distance
+        ,letter||''  AS  word
   FROM
        dfa_t
-  --WHERE letter IN ('F','P') -- constraints that a start state(node) has to have an edge on node 0
-  WHERE state_id =0 -- constraints that a start state(node) has to have an edge on node 0
+  WHERE state_id = 0 -- constraints that a start state(node) has to have an edge on node 0
   UNION
         SELECT
-               q.id,
-               d.letter,
-               d.successor,
+               d.state_id AS id,
+               d.letter AS l,
+               d.successor as suc,
                d.end_state AS end_s,
-               q.path_string || '-' || d.state_id AS path_string,
+               q.path_string || '-' || d.successor AS path_string,
                q.distance + 1 AS distance
             ,  q.word || d.letter AS word
         FROM dfa_t as d
             JOIN query AS q
                 --ON q.id = d.successor -- recursively checking what the next successor of q.b is, given q.b as predecessor.
-                ON d.state_id = q.suc
-    --WHERE "left"(q.word, 1) IN ('F','P')
-    --WHERE q.end_s != TRUE AND q.id = 0
-    --WHERE right(q.word, 1) = 't'
+                ON d.state_id = q.suc -- WHERE ("left"(word_input, 1) = "left"(word, 1))-- OR ("right"(word_input, 1) = "right"(word, 1))
+    --WHERE left(q.word, 1) IN ('F','P')
+    --WHERE right(q.word, 1) IN ('o', 'f', 'h') AND q.end_s = true
+    --WHERE d.end_state = TRUE AND d.state_id = 0
     --WHERE d.end_state = true
     --WHERE q.id = 0
-    --WHERE "left"(q.word, 1) IN ('P')
+    --WHERE "left"(q.word_input, length(q.word_input)) in ('Foto')
 ) SELECT word, path_string, distance FROM query
-WHERE end_s = true-- and distance > 0
+--WHERE id = 0 and end_s = true
+WHERE end_s = true
 ORDER BY distance;
+
+-- implementing the recursive query into a function with input parameter.
+-- the parameter "input" equals the string which patterns we are watching for in our table
+drop function if exists check_text_pattern(input char);
+create or replace function check_text_pattern(input char) RETURNS TABLE(first char, second char, path char, dist int) AS $$
+    WITH RECURSIVE query(id, l , suc, end_s, path_string, distance, word) AS (
+        SELECT
+               state_id as id,
+               letter as l,
+               successor as suc,                                   -- sb = the successor
+               end_state as end_s,
+               state_id ||'-'||successor AS path_string,    -- concatinating the first state with its successor
+               1 AS distance
+               ,letter||''  AS  word
+        FROM
+             dfa_t
+        WHERE state_id = 0 -- constraints that a start state(node) has to have an edge on node 0
+        --WHERE letter = "left"(input,1) -- constraints that a start state(node) has to have an edge on node 0
+        UNION
+        SELECT
+               d.state_id AS id,
+               d.letter AS l,
+               d.successor as suc,
+               d.end_state AS end_s,
+               q.path_string || '-' || d.successor AS path_string,
+               q.distance + 1 AS distance
+            ,  q.word || d.letter AS word
+        FROM dfa_t as d
+            JOIN query AS q
+                --ON q.id = d.successor -- recursively checking what the next successor of q.b is, given q.b as predecessor.
+                ON d.state_id = q.suc WHERE (right(left(input, q.distance),1) = q.l)-- OR ("right"(word_input, 1) = "right"(word, 1))
+        ) SELECT input, word, path_string, distance FROM query
+    WHERE end_s = true
+    ORDER BY distance;
+$$ LANGUAGE SQL;
+-- function with input pattern to be checked
+SELECT check_text_pattern('Photograph');
+
 
 
 ------------------------------------------------------------------------------------------------------------------------
@@ -122,7 +157,7 @@ SELECT *
 WHERE successor is Null;
 -- all
 SELECT * FROM dfa_t WHERE state_id IN (
-  SELECT s_a FROM transitions WHERE s_b = 42
+  SELECT s_a FROM transitions WHERE s_b is null
   UNION ALL
   SELECT s_b FROM transitions WHERE s_a = 0
 );
@@ -159,67 +194,42 @@ WITH RECURSIVE query(a, b, path_string, distance) AS (
 --WHERE query.word = 'Foto'--("left"('Foto', 1)  OR  query.word = 'Photo')
 ORDER BY distance;
 
-WITH RECURSIVE query(a, b, path_string, distance, word) AS (
-  SELECT
-         s_a,
-         s_b,
-         s_a ||''|| s_b AS path_string,
-         1 AS distance
-        ,letter || '' AS  word
-  FROM
-       dfa_t AS s, transitions AS t
-  --WHERE s_a = 0 -- constraints that a start state(node) has to have an edge on node 0
-  UNION
+------------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------EXPERIMENTAL--------------------------------------------------------
+-----------------------------------------------------FUNCTIONS----------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------
+
+drop function if exists check_text_pattern(input char);
+create or replace function check_text_pattern(input char) RETURNS TABLE(first char, second char, path char, dist int) AS $$
+    WITH RECURSIVE query(id, l , suc, end_s, path_string, distance, word) AS (
         SELECT
-               q.a,
-               t.s_b,
-               q.path_string || '' || t.s_b AS path_string,
+               state_id as id,
+               letter as l,
+               successor as suc,                                   -- sb = the successor
+               end_state as end_s,
+               state_id ||'-'||successor AS path_string,    -- concatinating the first state with its successor
+               1 AS distance
+               ,letter||''  AS  word
+        FROM
+             dfa_t
+        WHERE state_id = 0 -- constraints that a start state(node) has to have an edge on node 0
+        --WHERE letter = "left"(input,1) -- constraints that a start state(node) has to have an edge on node 0
+        UNION
+        SELECT
+               d.state_id AS id,
+               d.letter AS l,
+               d.successor as suc,
+               d.end_state AS end_s,
+               q.path_string || '-' || d.successor AS path_string,
                q.distance + 1 AS distance
-            ,  q.word || s.letter AS word
-        FROM dfa_t AS s, transitions AS t
+            ,  q.word || d.letter AS word
+        FROM dfa_t as d
             JOIN query AS q
-                ON t.s_a = q.b
-) SELECT * FROM query
-WHERE query.b = 4 AND (query.word = 'Foto' OR  query.word = 'Photo')-- checking if "Foto" and "Photo" is generated by the recursive query
--- WHERE query.word = 'Foto' OR query.word = 'Photo'
-ORDER BY distance;
-
-WITH RECURSIVE transitive_closure(actual_string) AS
-( SELECT letter ||'' AS actual_string
-  FROM dfa_t
-
-  UNION
-
-  SELECT tc.actual_string || n.letter AS actual_string
-  FROM dfa_t AS n
-    JOIN transitive_closure AS tc
-      ON n.letter = tc.actual_string
-  WHERE tc.actual_string NOT LIKE '%' || n.letter || '.%'
-)
-SELECT * FROM transitive_closure;
-
-
--- Tuple-F: list all end/finite acceptable dfa_t(nodes)
-SELECT *
-FROM   dfa_t s
-WHERE  NOT EXISTS (
-   SELECT  -- SELECT list mostly irrelevant; can just be empty in Postgres
-   FROM   transitions t
-   WHERE  t.s_a = s.state_id
-   );
-
-
-WITH RECURSIVE t(x) AS (
-  (SELECT 1 AS x
-
-  UNION
-
-  SELECT 1 AS x)
-
-  UNION ALL
-
-  SELECT x + 1
-  FROM   t
-  WHERE  x < 10
-
-) SELECT * FROM t;
+                --ON q.id = d.successor -- recursively checking what the next successor of q.b is, given q.b as predecessor.
+                ON d.state_id = q.suc WHERE (right(left(input, q.distance),1) = q.l)-- OR ("right"(word_input, 1) = "right"(word, 1))
+        ) SELECT input, word, path_string, distance FROM query
+    WHERE end_s = true
+    ORDER BY distance;
+$$ LANGUAGE SQL;
+-- function with input pattern to be checked
+SELECT check_text_pattern('Photograph');
